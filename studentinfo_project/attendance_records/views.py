b8152c386e8823da312043ad46ed26381629f2d5
pavi36-csbs <24ucb136pavithram@kgkite.ac.in>
2025-02-24 19:44:32 +0530

    django

--- studentinfo_project/attendance_records/views.py
+++ studentinfo_project/attendance_records/views.py
@@ -19,24 +19,25 @@ def login_view(request):
 
             if user is not None:
                 login(request,user)
-                return redirect(reverse("home:index"))
+                return redirect(reverse("attendance_records:index"))
             else:
                 return render(request,"attendance_records/login.html",{'title':'Login','form':form,'error':'Invalid Username or Password'})
 
     return render(request,"attendance_records/login.html",{'title':'Login','form':form,'style':'forms'})
 
 
-def index_view(request):
-    if request.user.is_authenticated:
-        return redirect(reverse("home:index"))
-    return render(request,"attendance_records/index.html",{'title':'Home'})
 
-@login_required(login_url='/login/')
+
+
 def index_view(request):
     '''attendance_data = Attendance.objects.all()
     return render(request,"attendance_records/index.html",{'title':'Home',"attendance_data":attendance_data})'''
-    return render(request,'index.html')
+    return render(request,'attendance_records/index.html')
    
 def logout_view(request):
     logout(request)
     return redirect(reverse("index"))
+
+def dashboard(request):
+    on_duties = OnDuty.objects.filter(student=request.user)
+    return render(request, 'attendance_records/dashboard.html', {'title': 'Dashboard', 'on_duties': on_duties})
