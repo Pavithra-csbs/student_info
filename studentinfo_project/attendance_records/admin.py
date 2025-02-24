33ee9d43e7d861f160f725332f9b00df0f436ac7
pavi36-csbs <24ucb136pavithram@kgkite.ac.in>
2025-02-24 19:43:46 +0530

    django

--- studentinfo_project/attendance_records/admin.py
+++ studentinfo_project/attendance_records/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import OnDuty
+class OnDutyAdmin(admin.ModelAdmin):
+    list_display = ('student', 'date', 'reason') 
+    search_fields = ('student__name', 'date')
 # Register your models here.
+admin.site.register(OnDuty, OnDutyAdmin)
