d1d4a48c8cf6729484f781bb21fd7615f6571615
pavi36-csbs <24ucb136pavithram@kgkite.ac.in>
2025-02-24 19:44:19 +0530

    django

--- studentinfo_project/attendance_records/urls.py
+++ studentinfo_project/attendance_records/urls.py
@@ -1,12 +1,14 @@
-from django.contrib.auth.views import LoginView, LogoutView
+
 from django.urls import path
-from .views import  login_view, index_view 
+from . import views
 
 app_name = 'attendance_records'
 
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='login.html'), name='log_in'),
-    path('home/', index_view, name='index'),
-    path('logout/', LogoutView.as_view(next_page="attendance_records:log_in"), name='logout'),
+    path('', views.login_view, name='login'),
+    path('index/', views.index_view, name='index'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
+    
 ]                                                                  
