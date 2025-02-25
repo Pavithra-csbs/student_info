9d49e616351b1d7f485ed5e21fcd83eda1768451
pavi36-csbs <24ucb136pavithram@kgkite.ac.in>
2025-02-25 12:06:00 +0530

    django

--- studentinfo_project/attendance_records/urls.py
+++ studentinfo_project/attendance_records/urls.py
@@ -8,7 +8,6 @@ app_name = 'attendance_records'
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('index/', views.index_view, name='index'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
     
 ]                                                                  
